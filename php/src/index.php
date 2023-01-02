145b2fa595faa34e4a60b8e4a926887e965a118e
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-02 11:31:30 -0600

    created two inputs for first and second numbers. created select with options for operator. created calculate button

--- php/src/index.php
+++ php/src/index.php
@@ -1,6 +1,10 @@
 <html>
-
+<head>
+<title>PHP Calculator</title>
+</head>
+<body>
 <!--Begin PHP with -->
+
 <?php
 
 echo 'PHP Calculator<br />';
@@ -8,6 +12,22 @@ echo 'PHP Calculator<br />';
 
 //End PHP with
 ?>
+
+<form>
+    <input type="text" name="number_one" placeholder="first number">
+    <input type="text" name="number_two" placeholder="second number">
+    <select name="operator">
+        <option></option>
+        <option>+</option>
+        <option>-</option>
+        <option>*</option>
+        <option>/</option>
+    </select>
+    <br />
+    <button type="submit" name="submit" value="submit">Calculate</button>
+
+</form>
+
 
 
 </body>
