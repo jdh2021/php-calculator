b2b0bcb9c24c962d6523759b5dfc6b630a39a994
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-03 10:10:59 -0600

    used switch statements changing operator to perform calculations

--- php/src/index.php
+++ php/src/index.php
@@ -6,17 +6,17 @@
 <!--Begin PHP with -->
 
 
-<h1>PHP Calculator</h1>
-<form>
+<h1>PHP Basic Calculator</h1>
+<form method="get">
     <input type="text" name="number_one" placeholder="first number">
-    <input type="text" name="number_two" placeholder="second number">
     <select name="operator">
-        <option></option>
         <option>+</option>
         <option>-</option>
         <option>*</option>
         <option>/</option>
     </select>
+    <input type="text" name="number_two" placeholder="second number">
+
     <br />
     <button type="submit" name="submit" value="submit">Calculate</button>
 </form>
@@ -24,14 +24,28 @@
 <h4> Result is: </h4>
 <?php
     // check if submit has been clicked
-    if(isset($_GET['submit'])) {
+    if (isset($_GET['submit'])) {
         // create variables to store number_one and number_two from URL
         $number_one=$_GET['number_one'];
-        $number_two=$_GET['number_two'];
+        $number_two=$_GET['number_twqo'];
         // create variable to store operator from URL
         $operator=$_GET['operator'];
+        // switch statement with case 
+        switch($operator) {
+            case "+":
+                echo $number_one + $number_two;
+            break;
+            case "-":
+                echo $number_one - $number_two;
+            break;
+            case "*":
+                echo $number_one * $number_two;
+            break;
+            case "/":
+                echo $number_one / $number_two;
+            break;
+        }
     }
-
 ?>
 
 </body>
