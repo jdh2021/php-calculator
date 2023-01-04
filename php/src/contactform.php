6ad6c3bbc9ce708d61bd73003a4bf498902f1c8e
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-04 14:56:47 -0600

    created contactform.php. conditional to check if 'send mail' was clicked. created variables to store data input by user in form

---
+++ php/src/contactform.php
@@ -0,0 +1,12 @@
+
+<?php
+
+// check if 'submit' was clicked
+if(isset($_POST['submit']) {
+    // variables to store data user entered in form
+    $name=$_POST['name'];
+    $subject=$_POST['subject'];
+    $mailFrom=$_POST['mail'];
+    $message=$_POST['message'];
+
+})
--- php/src/index.php
+++ php/src/index.php
@@ -50,11 +50,12 @@
 <h2>PHP Contact Form</h2>
     <p>Send Email</p>
     <form class="contact-form" action="contactform.php" method="post">
-    <input type="text" name="name" placeholder="Full Name">
-    <input type="text" name="mail" placeholder="Your Email">
-    <input type="text" name="subject" placeholder="Your Subject">
-    <textarea name="message" placeholder="Message"></textarea>
-
+        <input type="text" name="name" placeholder="Full Name">
+        <input type="text" name="mail" placeholder="Your Email">
+        <input type="text" name="subject" placeholder="Your Subject">
+        <textarea name="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">Send Mail</button>
+</form>
 </body>
 
 </html>
