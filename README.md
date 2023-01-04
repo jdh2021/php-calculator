fe190f248ce7a15d07d57db12669b9ebabbcd9d8
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-04 14:49:57 -0600

    updated README to include reference to smaller PHP projects including contact form. created inputs for contact form including name, email, subject, and message.

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# PHP Calculator
+## PHP Projects
+This repository stores small projects I'm working on using PHP and HTML with a Docker container. 
 
-I'm building a calculator using PHP and HTML with a Docker container. 
+### Calculator
+I built a calculator using PHP. The first number is input, an operator is selected from the dropdown, and a second number is input. Clicking `Calculate` produces the result of the calculation.
 
-## Setup
+### Contact Form
+I'm building a contact form with PHP based on [this](https://www.youtube.com/watch?v=4q0gYjAVonI&t=14s) tutorial from Dani Krossing. This form allows a message to be emailed from a webpage. 
+
+### Setup
 
 - Install [Docker](https://docs.docker.com/desktop/install/mac-install/) 
 - Run `docker-compose build` then `docker-compose up` in terminal. 
--- php/src/index.php
+++ php/src/index.php
@@ -1,12 +1,11 @@
 <html>
 <head>
-<title>PHP Calculator</title>
+<title>PHP Projects</title>
 </head>
 <body>
 <!--Begin PHP with -->
 
-
-<h1>PHP Basic Calculator</h1>
+<h2>PHP Basic Calculator</h2>
 <form method="get">
     <input type="text" name="number_one" placeholder="first number">
     <select name="operator">
@@ -16,7 +15,6 @@
         <option>/</option>
     </select>
     <input type="text" name="number_two" placeholder="second number">
-
     <br />
     <button type="submit" name="submit" value="submit">Calculate</button>
 </form>
@@ -27,7 +25,7 @@
     if (isset($_GET['submit'])) {
         // create variables to store number_one and number_two from URL
         $number_one=$_GET['number_one'];
-        $number_two=$_GET['number_twqo'];
+        $number_two=$_GET['number_two'];
         // create variable to store operator from URL
         $operator=$_GET['operator'];
         // switch statement with case 
@@ -46,7 +44,16 @@
             break;
         }
     }
+
 ?>
+
+<h2>PHP Contact Form</h2>
+    <p>Send Email</p>
+    <form class="contact-form" action="contactform.php" method="post">
+    <input type="text" name="name" placeholder="Full Name">
+    <input type="text" name="mail" placeholder="Your Email">
+    <input type="text" name="subject" placeholder="Your Subject">
+    <textarea name="message" placeholder="Message"></textarea>
 
 </body>
 
