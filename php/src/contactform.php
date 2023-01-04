5b56b56676e3b8290a762137a33b630fc9ea254a
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-04 15:37:22 -0600

    created variables mailTo, headers, and txt that will serve as parameters in mail function.

--- php/src/contactform.php
+++ php/src/contactform.php
@@ -1,12 +1,20 @@
 
 <?php
 
-// check if 'submit' was clicked
+// check if 'send mail' button was clicked
 if(isset($_POST['submit']) {
     // variables to store data user entered in form
     $name=$_POST['name'];
     $subject=$_POST['subject'];
     $mailFrom=$_POST['mail'];
     $message=$_POST['message'];
+
+    $mailTo="jhightowermn@gmail.com"
+    $headers="From: ".$mailFrom;
+    // customized message
+    $txt="You have received an email from ".$name.".\n\n".$message;
+
+    // mail method parameters - receiving email address, subject of email, message of email, headers
+    mail($mailTo, $subject, $txt, $headers);
 
 })
