84e90a5438ee9a45ad4638c612f860fd79c53bc3
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-19 09:02:07 -0600

    created second interface, LoginInterface, that is also implemented by Paypal and BankTransfer classes where loginFirst method is rquired

--- php/src/classes/payment.class.php
+++ php/src/classes/payment.class.php
@@ -4,26 +4,55 @@
 interface PaymentInterface {
     // rules classes should follow to use interface. class must have method payNow
     public function payNow();
+    public function paymentProcess();
+}
+
+interface LoginInterface {
+    public function loginFirst();
 }
 
 // implements - keyword for classes to follow rules of interface
-class Paypal implements PaymentInterface {
+class Paypal implements PaymentInterface, LoginInterface {
+    public function loginFirst() {}
     public function payNow() {}
+    // handler to decide how process will run
+    public function paymentProcess() {
+        $this->loginFirst();
+        $this->payNow();
+    }
 }
+
+class BankTransfer implements PaymentInterface, LoginInterface {
+    public function loginFirst() {}
+    public function payNow() {}
+    // handler to decide how process will run
+    public function paymentProcess() {
+        $this->loginFirst();
+        $this->payNow();
+    }
+}
+
 
 class Visa implements PaymentInterface {
     public function payNow() {}
+    public function paymentProcess() {
+        $this->payNow();
+    }
 }
 
 class Cash implements PaymentInterface {
     public function payNow() {}
+    public function paymentProcess() {
+        $this->payNow();
+    }
 }
 
 class BuyProduct {
-    // method to use a class above. use one keyword to reference three classes
+    // method to use a class above. interface to reference three classes that could be passed in
+    // refer to interface as type declaration
     public function pay(PaymentInterface $paymentType) {
         // reference properties and methods from all three classes inside method
-        $paymentType->payNow();
+        $paymentType->paymentProcess();
 
     }
 }
@@ -32,4 +61,5 @@ $paymentType = new Cash();
 $buyProduct = new BuyProduct();
 
 $buyProduct->pay($paymentType);
+
 ?>
